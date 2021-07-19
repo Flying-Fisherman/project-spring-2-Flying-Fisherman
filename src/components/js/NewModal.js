7e53284ee500f766944d0db529a1fd9b295ecde5
Flying-Fisherman <ghdud7037@gmail.com>
2021-07-20 00:35:11 +0900

    LogIn 로직 구성(미완성)
    - 로그인 기본 로직을 구성하였습니다.
    - 로그인이 안 된 상태에서는 로그인 버튼을, 로그인이 되었다면 환영 문구를 출력합니다.
    - 로그인 후에는 로그인 토큰과 로그인 한 `UserId`를 갖고 있습니다.

--- src/components/js/NewModal.js
+++ src/components/js/NewModal.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -30,13 +31,57 @@ function NewModal() {
       setOpen(false);
     };
 
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const userIdHandler = (e) => {
+        e.preventDefault();
+        setUserId(e.target.value);
+    };
+
+    const passwordHandler = (e) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+    };
+
+    let body = {
+        id: userId,
+        password: password,
+    };
+
+    const empty = () => {
+        setUserId("");
+        setPassword("");
+    }
+
+    const logIn = async () => {
+        axios.post('http://localhost:8080/session', body)
+        .then((res) => {
+        registerSuccessful(userId, res.data.token)
+        console.log(res)
+        });
+        setLog(true);
+    }
+
+    const registerSuccessful = (userId, token) => {
+        localStorage.setItem('token', token);
+        localStorage.setItem('authenticatedUser', userId);
+    }
+
+    const [log, setLog] = useState(false);
+
     return (
         <>
+        { log
 
-        <button type="button" onClick={handleOpen} className="btn btn-primary md-offset-6 md-2">
-          Log In
-        </button>
+        ? <div className="md-offset-6 md-2">
+          Hello! { localStorage.getItem('authenticatedUser') }
+          </div>
 
+        : <button type="button" onClick={handleOpen} className="btn btn-primary md-offset-6 md-2">
+            Log In
+          </button>
+        }
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
 
           <DialogTitle id="form-dialog-title">
@@ -52,6 +97,9 @@ function NewModal() {
              id="UserID"
              label="Your ID"
              fullWidth
+             value={userId}
+             onChange={userIdHandler}
+             required
             />
 
             <br/>
@@ -60,6 +108,9 @@ function NewModal() {
              id="UserPassword"
              label="password"
              type="password"
+             value={password}
+             onChange={passwordHandler}
+             required
             />
 
           </DialogContent>
@@ -68,8 +119,8 @@ function NewModal() {
             <Button onClick={handleClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleClose} color="primary">
-              Subscribe
+            <Button onClick={logIn} color="primary">
+              Log In
             </Button>
           </DialogActions>
 
